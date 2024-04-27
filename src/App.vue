14a3c2a2fb1d3bfc8a0611a76284b57770fd4d10
JoshTeflon <jenikele21h@gmail.com>
2024-04-27 11:31:00 +0100

    initial commit 🚀

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+// import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div></div>
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped></style>
---
+++ src/components/icons/AppLogo.vue
@@ -0,0 +1,35 @@
+<template>
+  <svg width="38" height="39" viewBox="0 0 38 39" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g filter="url(#filter0_d_1_380)">
+      <path
+        d="M10 28.5577V8.54262L27.7051 2L37.3268 5.07772V32.0212L27.7051 34.9225L10 28.5577L27.7051 30.6885V7.38897L16.1581 10.0835V25.8645L10 28.5577Z"
+        fill="#E1C596"
+      />
+    </g>
+    <defs>
+      <filter
+        id="filter0_d_1_380"
+        x="0.781705"
+        y="0.0246511"
+        width="36.545"
+        height="38.19"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dx="-6.5845" dy="0.65845" />
+        <feGaussianBlur stdDeviation="1.3169" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_380" />
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_380" result="shape" />
+      </filter>
+    </defs>
+  </svg>
+</template>
